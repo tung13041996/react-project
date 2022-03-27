b4d8072685ae60c895dd0eb654d51f2c3c030f31
Nguyen Thanh Tung <nguyenthanhtungbk96@gmail.com>
2022-03-27 21:40:13 +0700

    style toDoList

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,46 @@
+import { useState } from "react"
 import CountDownBox from "./assets/components/CountDownBox"
 import ToDoList from "./assets/components/ToDoList"
 
 function App() {
+
+  const [toDoList, setToDoList] = useState([
+    {
+      id: 1,
+      name: 'Task 1',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      name: 'Task 2',
+      isCompleted: true
+    }
+  ])
+
+  const handleAdd = (value) => {
+    setToDoList([
+      ...toDoList,
+        {
+            id: Date.now() + '-' + Math.random(),
+            name: value, 
+        }
+    ])
+  }
+
+  const handleCompleted = (task) => {
+    task.isCompleted = true
+    setToDoList([...toDoList]);
+}
+
+const handleUncompleted = (task) => {
+    task.isCompleted = false
+    setToDoList([...toDoList]);
+}
+
   return (
     <div className="App">
       {/* <CountDownBox name="Peter" number={0}/> */}
-      <ToDoList toDoList = {[
-        {
-          name: 'Task 1',
-          isCompleted: true
-        },
-        {
-          name: 'Task 2',
-          isCompleted: false
-        }
-      ]} />
+      <ToDoList toDoList = {toDoList} handleAdd={handleAdd} handleUncompleted={handleUncompleted} handleCompleted={handleCompleted}/>
     </div>
   )
 }
--- my-react-app/src/assets/components/ToDoList/index.jsx
+++ my-react-app/src/assets/components/ToDoList/index.jsx
@@ -1,40 +1,51 @@
-import React from 'react'
+import {React, useState} from 'react'
 import styles from './styles.scss'
 
-export default function ToDoList({toDoList}) {
+export default function ToDoList({toDoList, handleAdd, handleCompleted, handleUncompleted}) {
+
+    const [value, setValue] = useState('')
+
+  const _handleAdd = () => {
+      handleAdd(value);
+      setValue('')
+  }
+
+  const _handleCompleted = (task) => {
+    handleCompleted(task)
+}
+
+const _handleUncompleted = (task) => {
+    handleUncompleted(task)
+}
+
+
   return (
     <div className="todo-list">
         <div className="group-input">
-            <input type="text" placeholder="Add task..." />
-            <button>Add</button>
+            <input value={value} onChange = {event => setValue(event.target.value)} type="text" placeholder="Add task..." />
+            <button onClick={_handleAdd}>Add</button>
         </div>
 
-        <h2>All task need to do</h2>
-
         <div className="task-list">
-            <div className="task-done">
-                <h3>Task Done</h3>
-                {
-                    toDoList.map(e => {
-                        if (e.isCompleted) {
-                            <div className="task-item">
-                                <div className="task-item_name">{e.name}</div>
-                            </div>
-                        }
-                    })
-                }
-            </div>
-
             <div className="task-undone">
                 <h3>Task Undone</h3>
                 {
-                    toDoList.map(e => {
-                        if (e.isCompleted) {
-                            <div className="task-item">
-                                <div className="task-item_name">{e.name}</div>
-                            </div>
-                        }
-                    })
+                    toDoList.filter(e => !e.isCompleted).map(e => 
+                        <div key={e.id} className="task-item">
+                            <div onClick={_handleCompleted} className="task-item_name">{e.name}</div>
+                        </div>
+                    )
+                }
+            </div>
+
+            <div className="task-done">
+                <h3>Task Done</h3>
+                {
+                    toDoList.filter(e => e.isCompleted).map(e => 
+                        <div key={e.id} className="task-item">
+                            <div onClick={_handleUncompleted} className="task-item_name">{e.name}</div>
+                        </div>
+                    )
                 }
             </div>
         </div>
